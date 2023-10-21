2b94594b69558e1920e9815f046631525148a9c7
aryan0931 <aryan.yadav.mec22@itbhu.ac.in>
2023-10-21 09:52:18 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 import dill
 import pickle
@@ -22,7 +22,6 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -30,12 +29,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
-            
+
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
-            model.set_params(**gs.best_params_)
 
-           
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -53,4 +51,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)    
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
